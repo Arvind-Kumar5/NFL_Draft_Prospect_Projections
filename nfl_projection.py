422fcba1f9cbbe9ada120051e878229b7b84e39a
unknown <askumar3@wisc.edu>
2023-03-25 15:06:42 -0700

    added pro bowl request

--- nfl_projection.py
+++ nfl_projection.py
@@ -1,1 +1,63 @@
 #MAIN FILE
+import csv
+import pandas as pd
+import os
+import sys
+# need new column names to differentiate passing and rushing yards
+def changeTrainColNames(df):
+    cols = []
+    for colName in df.iloc[0,:]:
+        cols.append(colName)
+
+    passingStatsIndicies = range(5,14)
+    rushingStatsIndicies = range(14,18)
+
+    newCols = []
+    for i in range(len(cols)):
+        if i in passingStatsIndicies:
+            newCols.append("Passing_"+str(cols[i]))
+        elif i in rushingStatsIndicies:
+            newCols.append("Rushing_"+str(cols[i])) 
+        else:
+            newCols.append(cols[i])
+
+    df.columns = newCols
+    return newCols
+
+# some names have "*" next to them which needs to be removed
+def removeStarFromName(df):
+    index = 0
+    for name in df['Player']:
+        if "*" in name:
+            newName = name.split("*")[0]
+            df['Player'].values[index] = newName
+        index += 1
+
+def getTrainData(path):
+    dirs = os.listdir(path)
+    dfs = []
+    print("dirs: ", dirs)
+    for file in dirs:
+        if ".csv" in str(file):
+            try:
+                df = pd.read_csv(path+"/"+file) # read specfic csv file
+                df = df.drop("-additional", axis='columns') # drop unused column
+                changeTrainColNames(df) # specfic col names needed such as "Passing_Yds" and "Rushing_yds"
+                df = df.drop(0) # need to drop the column header row since we made new column header
+                removeStarFromName(df) # some names have a "*" next to them, need to remove the "*"
+
+                # drop unused column
+                df = df.drop("School", axis='columns') 
+                df = df.drop("Conf", axis='columns')
+                dfs.append(df)
+            except:
+                print(str(file) + " : CSV FILE ERROR")
+                continue
+           
+    bigDf = pd.concat(dfs)
+    return bigDf
+
+# 2010 data is 102 rows × 16 columns
+
+df = getTrainData("train_data")
+print(len(df))
---
+++ testCollectData.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import os
+import requests
+from bs4 import BeautifulSoup
+
+dfs = []
+for year in range(2010, 2022):
+    
+    url = "https://www.pro-football-reference.com/years/{}/probowl.htm"
+    url_year = url.format(year)
+    data = requests.get(url_year)
+
+    with open("proBowl2010.html", "w+") as f:
+        f.write(data.text)
+    with open("proBowl2010.html") as f:
+
+        html_page = f.read()
+
+    soup = BeautifulSoup(html_page, "html.parser")
+
+    pro_bowlers = soup.find(id="pro_bowl")
+    print(pro_bowlers)
+
+    #pro_bowlers = pd.read_html(str(pro_bowlers))[0]
+
+    #dfs.append(pro_bowlers)
+
+    os.remove("proBowl{}.html".format(year))
+
+pro_bowl = pd.concat(dfs)
+pro_bowl.drop(pro_bowl.loc[pro_bowl['Pos']!="QB"].index, inplace=True)
+print(pro_bowl)
