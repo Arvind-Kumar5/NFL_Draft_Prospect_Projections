5d4d96f0cb82f671983a6d8c96e62b0ceb99bf53
unknown <askumar3@wisc.edu>
2023-03-25 15:30:48 -0700

    Error handling

--- testCollectData.py
+++ testCollectData.py
@@ -4,6 +4,7 @@ import requests
 from bs4 import BeautifulSoup
 
 dfs = []
+rateLimit = False
 for year in range(2010, 2022):
     
     url = "https://www.pro-football-reference.com/years/{}/probowl.htm"
@@ -18,15 +19,20 @@ for year in range(2010, 2022):
 
     soup = BeautifulSoup(html_page, "html.parser")
 
-    pro_bowlers = soup.find(id="pro_bowl")
-    print(pro_bowlers)
+    try:
+        pro_bowlers = soup.find(id="pro_bowl")
 
-    pro_bowlers = pd.read_html(str(pro_bowlers))[0]
+        pro_bowlers = pd.read_html(str(pro_bowlers))[0]
 
-    dfs.append(pro_bowlers)
+        dfs.append(pro_bowlers)
+
+    except ValueError:
+        print("Rate Limit: Too many requests ve are blocked")
+        rateLimit = True
 
     os.remove("proBowl{}.html".format(year))
 
-pro_bowl = pd.concat(dfs)
-pro_bowl.drop(pro_bowl.loc[pro_bowl['Pos']!="QB"].index, inplace=True)
-print(pro_bowl)
+if rateLimit == False:
+    pro_bowl = pd.concat(dfs)
+    pro_bowl.drop(pro_bowl.loc[pro_bowl['Pos']!="QB"].index, inplace=True)
+    print(pro_bowl)
