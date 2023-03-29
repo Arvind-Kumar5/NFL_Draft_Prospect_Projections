c0f275158d4a1c914cee4ad2e6c76d1fbe4c972c
Aadhar Sharma <aadhar@aadhar.local>
2023-03-29 16:54:19 -0700

    made csv files

---
+++ makeCsv.py
@@ -0,0 +1,165 @@
+import pandas as pd
+import os
+import requests
+from bs4 import BeautifulSoup
+import re
+from tqdm import tqdm
+import pickle
+
+import time
+import sys
+
+probowlUrl = "https://www.pro-football-reference.com/years/{}/probowl.htm"
+probowlHtmlFile = "proBowl{}.html"
+probowlId = "pro_bowl" 
+
+collegeStatsUrl = "https://www.sports-reference.com/cfb/years/{}-passing.html"
+collegeStatsHtmlFile = "collegeStats{}.html"
+collegeStatsId = "div_passing"
+
+combineUrl = "https://www.pro-football-reference.com/draft/{}-combine.htm"
+combineHtmlFile = "combineStats{}.html"
+combineId = "div_combine" 
+
+def dots(i):
+    return "."*i
+
+def scrapeSite(url, htmlFileName, idName, startYear, endYear):
+    dfs = []
+    rateLimit = False
+    i = 1
+    for year in range(startYear, endYear+1):
+        url_year = url.format(year)
+        data = requests.get(url_year)
+
+        with open(htmlFileName.format(year), "w+") as f:
+            f.write(data.text)
+        with open(htmlFileName.format(year)) as f:
+
+            html_page = f.read()
+
+        soup = BeautifulSoup(html_page, "html.parser")
+
+        try:
+            df = soup.find(id=idName)
+            df = pd.read_html(str(df))[0]
+            dfs.append(df)
+
+            if i == 4:
+                print("")
+                i = 1
+            print("Collecting Data Vate (ve are not blocked)", dots(i), end='\r')
+            i = i + 1
+
+        except ValueError:
+            print("Rate Limit: Too many requests ve are blocked")
+            rateLimit = True
+
+        os.remove(htmlFileName.format(year))
+
+    return rateLimit, dfs 
+
+def getProbowl(rateLimit, dfs):
+    if rateLimit == False:
+        #append all pro bowl years together into dataframe
+        df = pd.concat(dfs)
+
+        df = df.reset_index(drop=True) # needed so that each row has unique index
+
+        # Drop all other positions except qb
+        df.drop(df.loc[df['Pos']!="QB"].index, inplace=True)
+        # print("only qb df: ", df)
+
+        #Clean data names and add to dict
+        regex = re.compile('[^a-zA-Z\s]')
+        playerNamesExctract = df.Player.tolist()
+        playerNames = {}
+        for name in playerNamesExctract:
+            playerNames[regex.sub('', name)] = True
+
+        return playerNames
+    return None
+
+# need new column names to differentiate passing and rushing yards
+def changeTrainColNames(college_stats_df):
+    new_cols = []
+
+    for i in range(len(college_stats_df.columns.values)):
+        col = college_stats_df.columns.values[i]
+        if i in range(0,5):
+            new_cols.append(col[len(col)-1])
+        else:
+            new_cols.append('_'.join(col))
+
+    college_stats_df.columns = new_cols
+
+# some names have "*" next to them which needs to be removed
+def removeStarFromName(df):
+    index = 0
+    for name in df['Player']:
+        if "*" in name:
+            newName = name.split("*")[0]
+            df['Player'].values[index] = newName
+        index += 1
+
+def getCollegeStatsDf(rateLimit, dfs):
+    if rateLimit == False:
+        df = pd.concat(dfs)
+        df = df.reset_index(drop=True) # needed so that each row has unique index
+        changeTrainColNames(df)
+        removeStarFromName(df)
+        return df
+    return None
+
+def getCombineDf(rateLimit, dfs):
+    if rateLimit == False:
+        df = pd.concat(dfs)
+        df = df.reset_index(drop=True) # needed so that each row has unique index
+        df.drop(index=df.loc[df['Pos']!='QB'].index, inplace=True)
+        df = df.drop(columns=['School', 'College', 'Drafted (tm/rnd/yr)'])
+        return df
+    return None
+
+# for training 2008 to 2022
+rateLimit, probowlDfs = scrapeSite(probowlUrl, probowlHtmlFile, probowlId, 2008, 2022)
+probowl = getProbowl(rateLimit, probowlDfs)
+
+print("\n")
+print("Pro-Bowlers:")
+print(probowl)
+print(len(probowl))
+
+with open('probowlers.pkl', 'wb') as fp:
+    pickle.dump(probowl, fp)
+    print('probowl dictionary saved successfully to file probowlers.pkl')
+print()
+
+# for training 2008 to 2017
+trainRateLimit, trainDfsStats = scrapeSite(collegeStatsUrl, collegeStatsHtmlFile, collegeStatsId, 2008, 2017) 
+# for validation 2018 to 2019
+valRateLimit, valDfsStats = scrapeSite(collegeStatsUrl, collegeStatsHtmlFile, collegeStatsId, 2018, 2019) 
+# for testing 2020 to 2021
+testRateLimit, testDfsStats = scrapeSite(collegeStatsUrl, collegeStatsHtmlFile, collegeStatsId, 2020, 2021) 
+
+trainDf = getCollegeStatsDf(trainRateLimit, trainDfsStats)
+valDf = getCollegeStatsDf(valRateLimit, valDfsStats)
+testDf = getCollegeStatsDf(testRateLimit, testDfsStats)
+
+# for training combine 2009 to 2018
+trainLimit, trainDfsCombine = scrapeSite(combineUrl,combineHtmlFile,combineId,2009,2018)
+# for validation combine 2019 to 2020
+valLimit, valDfsCombine = scrapeSite(combineUrl,combineHtmlFile,combineId,2019,2020)
+# for testing combine 2021 to 2022
+testLimit, testDfsCombine = scrapeSite(combineUrl,combineHtmlFile,combineId,2021,2022)
+
+combineTrainDf = getCombineDf(trainLimit, trainDfsCombine)
+combineValDf = getCombineDf(trainLimit, trainDfsCombine)
+combineTestDf = getCombineDf(trainLimit, trainDfsCombine)
+
+trainDf.to_csv('TrainData/trainDf.csv')
+valDf.to_csv('ValidationData/valDf.csv')
+testDf.to_csv('TestData/testDf.csv')
+
+combineTrainDf.to_csv('TrainData/combineTrainDf.csv')
+combineValDf.to_csv('ValidationData/combineValDf.csv')
+combineTestDf.to_csv('TestData/combineTestDf.csv')
--- nfl_projection.py
+++ nfl_projection.py
@@ -4,209 +4,100 @@ import requests
 from bs4 import BeautifulSoup
 import re
 from tqdm import tqdm
+import pickle
 
 import time
 import sys
 
-probowlUrl = "https://www.pro-football-reference.com/years/{}/probowl.htm"
-probowlHtmlFile = "proBowl{}.html"
-probowlId = "pro_bowl" 
+def getScore(row):
+    return ((((int(row["Passing_Cmp"])/int(row["Passing_Att"]))*100)) 
+            + (int(row["Passing_Yds"])/25) + (int(row["Passing_TD"])*4) 
+            + (int(row["Rushing_Yds"])/10) + (float(row["Passing_Rate"])/10.0) 
+            + (int(row["Rushing_TD"])*6)) - (int(row["Passing_Int"])*2)
 
-collegeStatsUrl = "https://www.sports-reference.com/cfb/years/{}-passing.html"
-collegeStatsHtmlFile = "collegeStats{}.html"
-collegeStatsId = "div_passing"
+def fillDfWithScores(trainDf):
+    for i in tqdm(range(10)):
+        # fill score column
+        for x, row in  trainDf.iterrows():
+            if row['Player'] == 'Player':
+                continue
+            try:
+                row['Score'] = getScore(row)
 
-combineUrl = "https://www.pro-football-reference.com/draft/{}-combine.htm"
-combineHtmlFile = "combineStats{}.html"
-combineId = "div_combine" 
+            except ValueError:
+                print("error on this row: \n", row)
+                break
 
-def dots(i):
-    return "."*i
+        time.sleep(0.5)
 
-def printCollectingData():
-    pass
+def getDuplicates(duplicateDf):
+    duplicates = {}
 
-def scrapeSite(url, htmlFileName, idName, startYear, endYear):
-    dfs = []
-    rateLimit = False
-    i = 1
-    for year in range(startYear, endYear+1):
-        url_year = url.format(year)
-        data = requests.get(url_year)
-
-        with open(htmlFileName.format(year), "w+") as f:
-            f.write(data.text)
-        with open(htmlFileName.format(year)) as f:
-
-            html_page = f.read()
-
-        soup = BeautifulSoup(html_page, "html.parser")
-
-        try:
-            df = soup.find(id=idName)
-            df = pd.read_html(str(df))[0]
-            dfs.append(df)
-            #print("request gord. ve are not blocked")
-            if i == 4:
-                print("")
-                i = 1
-
-            print("Collecting Data Vate (ve are not blocked)", dots(i), end='\r')
-            i = i + 1
-        except ValueError:
-            print("Rate Limit: Too many requests ve are blocked")
-            rateLimit = True
-
-        
-        
-        os.remove(htmlFileName.format(year))
-
-    return rateLimit, dfs 
-
-def getProbowl(rateLimit, dfs):
-    if rateLimit == False:
-        #append all pro bowl years together into dataframe
-        df = pd.concat(dfs)
-
-        df = df.reset_index(drop=True) # needed so that each row has unique index
-
-        # Drop all other positions except qb
-        df.drop(df.loc[df['Pos']!="QB"].index, inplace=True)
-        # print("only qb df: ", df)
-
-        #Clean data names and add to dict
-        regex = re.compile('[^a-zA-Z\s]')
-        playerNamesExctract = df.Player.tolist()
-        playerNames = {}
-        for name in playerNamesExctract:
-            playerNames[regex.sub('', name)] = True
-
-        return playerNames
-    return None
-
-# need new column names to differentiate passing and rushing yards
-def changeTrainColNames(college_stats_df):
-    new_cols = []
-
-    for i in range(len(college_stats_df.columns.values)):
-        col = college_stats_df.columns.values[i]
-        if i in range(0,5):
-            new_cols.append(col[len(col)-1])
-        else:
-            new_cols.append('_'.join(col))
-
-    college_stats_df.columns = new_cols
-
-# some names have "*" next to them which needs to be removed
-def removeStarFromName(df):
-    index = 0
-    for name in df['Player']:
-        if "*" in name:
-            newName = name.split("*")[0]
-            df['Player'].values[index] = newName
-        index += 1
-
-def getCollegeStatsDf(rateLimit, dfs):
-    if rateLimit == False:
-        df = pd.concat(dfs)
-        df = df.reset_index(drop=True) # needed so that each row has unique index
-        changeTrainColNames(df)
-        removeStarFromName(df)
-        return df
-    return None
-
-def getCombineDf(rateLimit, dfs):
-    if rateLimit == False:
-        df = pd.concat(dfs)
-        df = df.reset_index(drop=True) # needed so that each row has unique index
-        df.drop(index=df.loc[df['Pos']!='QB'].index, inplace=True)
-        df = df.drop(columns=['School', 'College', 'Drafted (tm/rnd/yr)'])
-        return df
-    return None
-
-# for training 2008 to 2017
-trainRateLimit, trainDfsProbowl = scrapeSite(probowlUrl, probowlHtmlFile, probowlId, 2008, 2017)
-# for validation 2018 to 2019
-#valRateLimit, valDfsProbowl = scrapeSite(probowlUrl, probowlHtmlFile, probowlId, 2018, 2019)
-# for testing 2020 to 2021
-#testRateLimit, testDfsProbowl =  scrapeSite(probowlUrl, probowlHtmlFile, probowlId, 2020, 2021)
-
-trainProbowl = getProbowl(trainRateLimit, trainDfsProbowl)
-#valProbowl= getProbowl(valRateLimit, valDfsProbowl)
-#testProbowl = getProbowl(testRateLimit, testDfsProbowl)
-
-print("Pro-Bowlers: \n")
-print(trainProbowl)
-print(len(trainProbowl))
-
-
-# for training 2008 to 2017
-trainRateLimit, trainDfsStats = scrapeSite(collegeStatsUrl, collegeStatsHtmlFile, collegeStatsId, 2008, 2017) 
-# for validation 2018 to 2019
-#valRateLimit, valDfsStats = scrapeSite(collegeStatsUrl, collegeStatsHtmlFile, collegeStatsId, 2018, 2019) 
-# for testing 2020 to 2021
-#testRateLimit, testDfsStats = scrapeSite(collegeStatsUrl, collegeStatsHtmlFile, collegeStatsId, 2020, 2021) 
-
-trainDf = getCollegeStatsDf(trainRateLimit, trainDfsStats)
-#valDf = getCollegeStatsDf(valRateLimit, valDfsStats)
-#testDf = getCollegeStatsDf(testRateLimit, testDfsStats)
-
-
-# for training combine 2009 to 2018
-trainLimit, trainDfsCombine = scrapeSite(combineUrl,combineHtmlFile,combineId,2009,2018)
-# for validation combine 2019 to 2020
-## TODO
-# for testing combine 2021 to 2022
-## TODO
-combineTrainDf = getCombineDf(trainLimit, trainDfsCombine)
-
-# add score column with everything preset to 0
-trainDf['Score'] = None
-score = 0
-
-for i in tqdm(range(10)):
-
-    # fill score column
-    for x, row in  trainDf.iterrows():
+    for x, row in duplicateDf.iterrows():
         if row['Player'] == 'Player':
             continue
-        try:
-            score = ((((int(row["Passing_Cmp"])/int(row["Passing_Att"]))*100)) + (int(row["Passing_Yds"])/25) + (int(row["Passing_TD"])*4) + (int(row["Rushing_Yds"])/10) + (float(row["Passing_Rate"])/10.0) + (int(row["Rushing_TD"])*6)) - (int(row["Passing_Int"])*2)
-            row['Score'] = score
-
-        except ValueError:
-            print("error on this row: \n", row)
-            break
-
-    time.sleep(0.5)
-
-duplicateDf = trainDf[trainDf.Player.duplicated(keep=False)].sort_values("Player")
-
-duplicates = {}
-
-for x, row in duplicateDf.iterrows():
-
-    if row['Player'] == 'Player':
-        continue
-
-    elif row['Player'] not in duplicates:
-        duplicates[row['Player']] = dict(row)
-
-    else:
-
-        if row['Score'] > duplicates[row['Player']]['Score']:
-
+        elif row['Player'] not in duplicates:
             duplicates[row['Player']] = dict(row)
+        else:
+            if row['Score'] > duplicates[row['Player']]['Score']:
+                duplicates[row['Player']] = dict(row)
+    
+    return duplicates
 
-print(duplicates["Derek Carr"])
-bestScoreDuplicates = []
-for player in duplicates:
-    trainDf.drop(trainDf.loc[trainDf['Player']==player].index, inplace=True)
-    bestScoreDuplicates.append(pd.DataFrame([duplicates[player]]))
-    # trainDf = trainDf.append(duplicates[player], ignore_index = True)
+# update df such that only duplicates with the best scores are used 
+def updateDfNoDuplicates(trainDf, duplicates):
+    bestScoreDuplicates = []
 
-trainDf = pd.concat(bestScoreDuplicates, ignore_index = True)
-print("No duplicates \n")
+    for player in duplicates:
+        trainDf.drop(trainDf.loc[trainDf['Player']==player].index, inplace=True)
+        bestScoreDuplicates.append(pd.DataFrame([duplicates[player]]))
+
+    trainDf = pd.concat(bestScoreDuplicates, ignore_index = True)
+
+    return trainDf
+
+# read from the csv files and create a df
+trainDf = pd.read_csv('TrainData/trainDf.csv') #, index_col=0) -> ignore for now
+combineTrainDf = pd.read_csv('TrainData/combineTrainDf.csv') #, index_col=0) -> ignore for now
+
+print()
+# needed for some formatting issues when reading csv
+trainDf.drop(trainDf.columns[trainDf.columns.str.contains('unnamed',case = False)],axis = 1, inplace = True)
+print("----------- trainDf:")
 print(trainDf)
+print()
 
-time.sleep(60)
+combineTrainDf.drop(combineTrainDf.columns[combineTrainDf.columns.str.contains('unnamed',case = False)],axis = 1, inplace = True)
+# needed for some formatting issues when reading csv
+print("----------- combineTrainDf:")
+print(combineTrainDf)
+print()
+
+probowlers = {}
+with open('probowlers.pkl', 'rb') as fp:
+    probowlers = pickle.load(fp)
+
+if not probowlers:
+    print("ve are in a bit of trouble")
+print("----------- probowlers: ", probowlers)
+print("----------- probowlers len: ", len(probowlers))
+print()
+
+# add score column with everything preset to None
+trainDf['Score'] = None
+
+# calculate the score for each row and add it to the row
+fillDfWithScores(trainDf)
+
+# get the duplicate players (players with multiple college season stats)
+duplicateDf = trainDf[trainDf.Player.duplicated(keep=False)].sort_values("Player")
+duplicates = getDuplicates(duplicateDf)
+print("----------- duplicates[\"Derek Carr\"]: ")
+print(duplicates["Derek Carr"])
+print()
+
+# create trainDf with no duplicates 
+trainDf = updateDfNoDuplicates(trainDf, duplicates)
+print("----------- No duplicates train df")
+print(trainDf)
+print()
